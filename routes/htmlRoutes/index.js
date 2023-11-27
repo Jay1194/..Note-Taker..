b0b3efd40cc72fe9647f9109c44b81ab93859f1d
Jay <j.thomastt1@gmail.com>
2023-11-26 20:55:33 -0600

    updated

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,9 +1,9 @@
- const path = require('path');
+ const path = require('');
 
  const router = require('express').Router();
 
  router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
  });
 
  module.exports = router;
