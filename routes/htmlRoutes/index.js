dc934e5e533d3048cf5176e09e1d65ce508f7c49
Jay <j.thomastt1@gmail.com>
2023-11-26 21:10:50 -0600

    front and back end connected

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,6 +6,14 @@
     res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
  });
 
+ router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/notes.html'))
+ })
+
+ router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
+});
+
  module.exports = router;
 
  
