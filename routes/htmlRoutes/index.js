62b867e339211aad9734f517e31732aa887519f1
Jay <j.thomastt1@gmail.com>
2023-11-26 20:57:53 -0600

    heroku work

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,7 +3,7 @@
  const router = require('express').Router();
 
  router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
  });
 
  module.exports = router;
